092df48fe05e9d4325cd626567f5daa8cf5aecbc
Erika Perugachi <erikaperugachi@gmail.com>
2016-06-10 15:57:20 -0500

    Add reload data manually to image bubble

--- components/BubbleImage.js
+++ components/BubbleImage.js
@@ -3,45 +3,53 @@ import React, { Component } from 'react'
 class BubbleImage extends Component {
 	constructor(props) {
 		super(props);
-
+		this.state = {
+			isDownloading: false
+		}
+		this.eventBubble = this.eventBubble.bind(this);
 		this.openImage = this.openImage.bind(this);
 		this.downloadData = this.downloadData.bind(this);
 	}
 
-	componentWillMount() {		
-        if(this.props.message.data == null && !this.props.message.isDownloading){
+	componentWillMount() {
+        if(this.props.message.data == null && !this.state.isDownloading && !this.props.message.error){
             this.props.dataDownloadRequest(this.props.message.mokMessage);
-            this.props.message.isDownloading = true;
+            this.setState({isDownloading: true});
         }
 	}
-
+	
 	render() {
-		// console.log(this.props.message);
 		return (
-			<div className="mky-content-image">
+			<div className='mky-content-image'>
 				{ this.props.message.data
-					? (
-						<div className="mky-content-image-data">
+					? ( <div className='mky-content-image-data'>
 							<img src={this.props.message.data} onClick={this.openImage}></img>
 						</div>
 					)
-					: (
-                        <div className='mky-content-image-loading'>
-                            <div className='mky-double-bounce1'></div>
-                            <div className='mky-double-bounce2'></div>
-                        </div>
+					: ( this.state.isDownloading
+						? ( <div className='mky-content-image-loading'>
+                            	<div className='mky-double-bounce1'></div>
+								<div className='mky-double-bounce2'></div>
+							</div>
+						)
+						: <div className='mky-content-image-to-download' onClick={this.downloadData}><i className='demo-icon mky-menu-down'>&#xe815;</i></div>
                     )
                 }
 			</div>
 		)
 	}
-
+	
 	openImage() {
 		this.props.messageSelected(this.props.message);
 	}
 
-	downloadData() {
+	eventBubble() {
 		this.props.onClickMessage(this.props.message.mokMessage);
+	}
+	
+	downloadData() {
+		this.props.dataDownloadRequest(this.props.message.mokMessage);
+        this.setState({isDownloading: true});
 	}
 }
 
