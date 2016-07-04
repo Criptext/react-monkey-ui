52ee7de3699e3f6cd5c30169ed28eb3cc7f6819b
Raquel Villon <raqevill@espol.edu.ec>
2016-07-04 18:12:39 -0500

    file input type and auto size textarea, still missing an autoscroll

--- components/InputMenu.js
+++ components/InputMenu.js
@@ -10,7 +10,7 @@ class InputMenu extends React.Component {
 			<div id="mky-menu-bubble" className="mky-menu-bubble" style={{display : this.props.visible ? "block" : "none"}}>
 				{/*<div className="menu-bubble-item" onClick={this.props.enableGeoInput}><i id="mky-menu-location-icon" className="demo-icon mky-location">&#xe815;</i><p>Send Location</p></div>*/}
 				<div className="mky-menu-bubble-item" onClick={this.props.handleAttach}><i id="mky-menu-attach-icon" className="icon mky-icon-image-regular" style={this.props.colorButton}></i> <div>Image</div></div>
-				<div className="mky-menu-bubble-item" onClick={this.props.handleAttach}><i id="mky-menu-attach-file" className="icon mky-icon-doc-regular" style={this.props.colorButton}></i><div>File</div></div>
+				<div className="mky-menu-bubble-item" onClick={this.props.handleAttachFile}><i id="mky-menu-attach-file" className="icon mky-icon-doc-regular" style={this.props.colorButton}></i><div>File</div></div>
 				<div id="mky-layer-menu" onClick={this.props.toggleVisibility} ></div>
 			</div>
 		)
