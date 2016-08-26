97ee008e477fce4a47079cb445f8a4d3bb6f7a91
Erika Perugachi <erikaperugachi@gmail.com>
2016-08-26 18:31:43 -0500

    Fix structure info panel

--- components/ContentInfo.js
+++ components/ContentInfo.js
@@ -5,7 +5,7 @@ import InfoItem from './InfoItem.js';
 class ContentInfo	 extends Component {
 	constructor(props, context) {
 		super(props, context);
-		this.classContent = this.props.isMobile ? 'mky-expand-each-screen' : 'mky-no-expand';
+		this.classExpand = this.props.isMobile ? 'mky-content-info-expand' : 'mky-content-info-no-expand';
 		this.objectInfo = {};
 		this.state = {
 			editingName : false, 
@@ -19,73 +19,74 @@ class ContentInfo	 extends Component {
 
 	componentWillMount(){
 		this.objectInfo = this.props.getConversationInfo();
-		this.setState({
-			infoName : this.objectInfo.name
-		});
+		this.setState({infoName: this.objectInfo.name});
 	}
 
 	componentWillReceiveProps(nextProps) {
 		this.objectInfo = this.props.getConversationInfo();
 		if(this.objectInfo.name != this.state.infoName && !this.state.editingName){
-			this.setState({
-				infoName : this.objectInfo.name
-			})
+			this.setState({infoName: this.objectInfo.name})
 		}
 	}
 
 	render() {
-
+	
     	return (
-			<aside className={this.props.isMobile || this.props.viewType != 'fullscreen' ? "mky-aside-right-wide" : "mky-aside-right"} >
-				<header className="mky-info-header" >
-
+			<aside className={'mky-content-info '+this.classExpand} >
+				<header className='mky-info-header'>
 					<div className='mky-info-back' onClick={this.props.toggleConversationHeader}><i className="icon mky-icon-close" style={{fontSize : '14px', marginTop : '5px', display : 'block'}}></i></div>
-
 					<div className="mky-info-header-block" style={{marginLeft : '20px'}}>
-						<span className='mky-ellipsify mky-info-header-name'>{this.objectInfo.title ? this.objectInfo.title : "Information"}</span>
+						<span className='mky-ellipsify mky-info-header-name'>{this.objectInfo.title ? this.objectInfo.title : 'Information'}</span>
 						<span className="mky-info-header-desc" ></span>
 					</div>
-					
 				</header>
-				<div className="mky-info-container">
-					<div className="mky-info-body-img">
-					  <img src={this.objectInfo.avatar} />
+				<div className='mky-info-container'>
+					<div className='mky-info-conversation-image'>
+						<img src={this.objectInfo.avatar} />
 					</div>
-					<div className="mky-info-input" >
-						<label>Name</label>
-						<input ref="nameChange" value={this.state.infoName} onChange={this.handleNameChange} onKeyDown={this.handleNameKeyDown} onBlur={this.handleNameBlur} type="text" className="mky-info-input-input" disabled={this.state.editingName ? false : true}/>
-						{this.objectInfo.renameGroup && !this.state.editingName ? <i className="icon mky-icon-edit mky-info-edit-icon" onClick={this.toogleEditName}></i> : null}					
-					</div>
-					<div className="mky-info-subtitle">
-						<div className="mky-info-subtitle-head">
-							<div className="mky-info-subtitle-left">
-								{this.objectInfo.subTitle}
-							</div>
-							<div>
-								{this.objectInfo.users.length + " of 50"}
+					<div className='mky-info-conversation-description'>
+						<div className='mky-info-conversation-header'>
+							<label className='mky-info-conversation-title'>Name</label>
+							<div className='mky-info-conversation-action'>
+								<input ref='nameChange'
+									className='mky-info-input'
+									value={this.state.infoName}
+									onChange={this.handleNameChange}
+									onKeyDown={this.handleNameKeyDown}
+									onBlur={this.handleNameBlur}
+									type='text'
+									disabled={this.state.editingName ? false : true}/>
+								{ this.objectInfo.renameGroup && !this.state.editingName ? <i className='icon mky-icon-edit mky-info-edit-icon' onClick={this.toogleEditName}></i> : null }	
 							</div>
 						</div>
-						{this.objectInfo.canAdd 
-							?	<div>
-									<i className="icon mky-icon-add-regular"></i> Add {this.objectInfo.users && this.objectInfo.users.length > 0 ? this.objectInfo.subTitle : null}
-								</div>
-							: null
-						}
 					</div>
-					<div className="mky-info-list">
-						{this.objectInfo.users ? this.renderList(this.objectInfo.users, this.objectInfo.actions) : null}
+					<div className='mky-info-conversation-description mky-info-conversation-adapt'>
+						<div className='mky-info-conversation-header'>
+							<label className='mky-info-conversation-title'>{this.objectInfo.subTitle}</label>
+							<span className='mky-info-conversation-amount'>{this.objectInfo.users.length + ' of 50'}</span>
+							<div className='mky-info-conversation-action'>
+								{ this.objectInfo.canAdd 
+									? <div>
+										<i className='icon mky-icon-add-regular'></i> Add {this.objectInfo.users && this.objectInfo.users.length > 0 ? this.objectInfo.subTitle : null }
+									</div>
+									: null
+								}
+							</div>
+						</div>
+						<div className='mky-info-conversation-container'>
+							<div className='mky-info-conversation-list'>
+								{ this.objectInfo.users ? this.renderList(this.objectInfo.users, this.objectInfo.actions) : null }
+							</div>
+						</div>
 					</div>
-
 					{ this.objectInfo.button && this.objectInfo.button.text && this.objectInfo.button.func
 						? <button className="mky-info-button" onClick={ () => { this.objectInfo.button.func(this.props.conversationSelected.id) } }>
-							<span className="mky-info-button-text">
-								{this.objectInfo.button.text}
-							</span>
-							<i className={this.objectInfo.button.class ? this.objectInfo.button.class : "icon mky-icon-signout-sober mky-info-button-i"}></i>
+							<span className="mky-info-button-text">{this.objectInfo.button.text}</span>
+							<i className={ this.objectInfo.button.class ? this.objectInfo.button.class : 'icon mky-icon-signout-sober mky-info-button-i'}></i>
 						</button>
 						: null
 					}
-			  	</div>
+				</div>
 			</aside>
 		)
 	}
@@ -138,7 +139,6 @@ class ContentInfo	 extends Component {
 			editingName : false
 		});
 	}
-
 }
 
 ContentInfo.contextTypes = {
--- components/ContentWindow.js
+++ components/ContentWindow.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react'
 import ContentIntro from './ContentIntro.js';
 import ContentBanner from './ContentBanner.js';
 import ContentConversation from './ContentConversation.js';
+import ContentInfo from './ContentInfo.js';
 
 class ContentWindow extends Component {
 	constructor(props){
 		super(props);
+		this.state = {
+			showConversationInfo: false
+		}
 		this.classExpand = this.props.isMobile ? 'mky-expand-each-screen' : 'mky-content-window-with';
 		this.classStateWindow = 'mky-disabled';
 		this.classWithBanner = this.props.showBanner && !this.props.isMobile ? 'content-window-with-divided' : '';
+		
+		this.toggleConversationHeader = this.toggleConversationHeader.bind(this);
 	}
 
 	componentWillMount() {
@@ -16,28 +22,27 @@ class ContentWindow extends Component {
 			this.classExpand = 'mky-content-window-only';
 		}
 	}
+	
+	componentWillReceiveProps(nextProps) {
+		if(nextProps.conversationSelected && this.props.conversationSelected){
+			if(nextProps.conversationSelected.id && nextProps.conversationSelected.id != this.props.conversationSelected.id) {
+				this.toggleConversationHeader();
+			}
+		}
+	}
 
 	render() {
 		if(this.props.conversationSelected != undefined){
 			this.classStateWindow = '';
 		}
-		if(this.props.showConversationInfo){
-			if(this.props.viewType === 'fullscreen'){
-				this.classThinner = 'mky-chat-thinner';	
-			}else{
-				this.classThinner = 'mky-disappear';
-			}
-		}else {
-			this.classThinner = '';
-		}
 
     	return (
-	    	<section className={this.classExpand+' '+this.classStateWindow + ' '+this.classWithBanner + ' ' + this.classThinner}>
+	    	<section className={this.classExpand+' '+this.classStateWindow + ' '+this.classWithBanner}>
 	    	{ this.props.conversationSelected
 		    	? <ContentConversation closeSide={this.props.closeSide}
 		    		viewType={this.props.viewType}
-		    		showConversationInfo = {this.props.showConversationInfo}
-		    		toggleConversationHeader = {this.props.toggleConversationHeader}
+		    		showConversationInfo = {this.state.showConversationInfo}
+		    		toggleConversationHeader = {this.toggleConversationHeader}
 		    		customLoader={this.props.customLoader}
 		    		connectionStatus={this.props.connectionStatus}
 		    		handleNotifyTyping={this.props.handleNotifyTyping}
@@ -57,13 +62,24 @@ class ContentWindow extends Component {
 		    		version={this.props.version}/>
 		    	: <ContentIntro isMobile={this.props.isMobile} showBanner={this.props.showBanner}/>
 	    	}
-				{
-					this.props.showBanner && !this.props.isMobile ?
-						<ContentBanner />
-						:null
-				}
+	    	{ this.state.showConversationInfo
+		    	? <ContentInfo toggleConversationHeader = {this.toggleConversationHeader}		
+            		getConversationInfo = {this.props.getConversationInfo}
+            		isMobile={this.props.isMobile}
+            		conversationSelected={this.props.conversationSelected}
+            		viewType={this.props.viewType}/>
+            	: null
+	    	}
+			{ this.props.showBanner && !this.props.isMobile ?
+				<ContentBanner />
+				: null
+			}
 			</section>
 		);
+	}
+	
+	toggleConversationHeader() {
+		this.setState({showConversationInfo: !this.state.showConversationInfo});
 	}
 }
 
