99c23eeb1f0ca29b8e10d938a4f87b17df029d73
Erika Perugachi <erikaperugachi@gmail.com>
2016-12-30 16:17:13 -0500

    Add language

--- components/ContentLogOut.js
+++ components/ContentLogOut.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
+import Lang from '../lang'
 
 class ContentLogOut extends Component {
 
-	constructor(props){
-		super(props);
+	constructor(props, context){
+		super(props, context);
 	}
 	
 	render() {
-    	return <button className='mky-popup-button' onClick={this.props.userSessionLogout}>YES</button>
+    	return <button className='mky-popup-button' onClick={this.props.userSessionLogout}>{Lang[this.context.lang]['button.yes.text']}</button>
 	}
+}
+
+ContentLogOut.contextTypes = {
+    lang: React.PropTypes.string.isRequired
 }
 
 export default ContentLogOut;
