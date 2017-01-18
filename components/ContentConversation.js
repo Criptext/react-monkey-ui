e49bc1d456907acb5b23d0200a8f6a587c8688a8
Erika Perugachi <erikaperugachi@gmail.com>
2017-01-18 16:57:43 -0500

    Handle form

--- components/ContentConversation.js
+++ components/ContentConversation.js
@@ -6,7 +6,7 @@ import Modal from './Modal.js'
 import Panel from './Panel.js'
 import ContentViewer from './ContentViewer.js'
 import ContentReconnect from './ContentReconnect'
-import { defineTime, defineTimeByDay, getContrastColorObject } from '../utils/monkey-utils.js'
+import { defineTime, defineTimeByDay, getCombineColor, getContrastColorObject } from '../utils/monkey-utils.js'
 import Lang from '../lang'
 
 const Modal_ = Modal(ContentViewer);
@@ -155,12 +155,13 @@ class ContentConversation extends Component {
 				style.header.background = this.context.styles.toggleColor;	
 				style.header.borderBottom = '1px solid ' + this.context.styles.toggleColor;
 				style.optionButton = getContrastColorObject(this.context.styles.toggleColor);
+				style.subtitle.color = getCombineColor(this.context.styles.toggleColor);
 			}
 			if(this.context.styles.subtitleTextColor){
 				style.subtitle.color = this.context.styles.subtitleTextColor;
 			}
-			if(this.context.styles.titleTextColor){
-				style.title.color = this.context.styles.titleTextColor
+			if(this.context.styles.tabTextColor){
+				style.title.color = this.context.styles.tabTextColor
 			}
 		}
 		
